c16a0cd7b989fd0dd0ff6f71c34ba9c7055ebd4f
Ryan Michael <ryan@bcarc.com>
2010-04-13 17:08:06 -0500

    adding join table for tags

--- db/migrate/20100413213732_create_tags.rb
+++ db/migrate/20100413213732_create_tags.rb
@@ -5,9 +5,18 @@ class CreateTags < ActiveRecord::Migration
 
       t.timestamps
     end
+    
+    # generate the join table
+    create_table :tags_projects, :id => false do |t|
+      t.column :tag_id, :integer
+      t.column :project_id, :integer
+    end
+    add_index "tags_projects", "tag_id"
+    add_index "tags_projects", "project_id"
   end
 
   def self.down
     drop_table :tags
+    drop_table :tags_projects
   end
 end
