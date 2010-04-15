91335422ef979acb82a054e4cf339ec0006cb7a2
Ryan Michael <ryan@bcarc.com>
2010-04-15 15:57:58 -0500

    initial import of sql data from TG (adding sqlite db for now)

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   config.gem "searchlogic"
   config.gem "acts_as_markup"
   config.gem "right_aws"
+  config.gem "mysql"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- db/migrate/20100413213923_create_plans.rb
+++ db/migrate/20100413213923_create_plans.rb
@@ -3,7 +3,12 @@ class CreatePlans < ActiveRecord::Migration
     create_table :plans do |t|
       t.string :name
       t.integer :position
-      
+
+      t.string :attachment_file_name
+      t.string :attachment_content_type
+      t.integer :attachment_file_size
+      t.datetime :attachment_updated_at
+            
       t.belongs_to :project
 
       t.timestamps
