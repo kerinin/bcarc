01665779139ed4b93586f84f7175d383b42e1e8f
Ryan Michael <kerinin@gmail.com>
2010-04-17 11:07:52 -0500

    project#show displaying thumbnails

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,5 @@
 module ProjectsHelper
   def project_image( image )
+    image_tag image.attachment.url(:thumb), :size => '55x40', :swapover => image_path( image.attachment.url(:thumb) )
   end
-  
 end
