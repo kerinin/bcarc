7fb981bb61c66f26b09469d6612cc74035ddac59
Ryan Michael <kerinin@gmail.com>
2010-04-17 10:49:32 -0500

    fixing image display for index (and some others too)

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   has_attached_file :attachment, 
-    :styles => { :thumb => '100x100>', :index => '380x200#', :full => '800x800>' }, 
+    :styles => { :thumb => '55x40#', :index => '390x180#', :full => '800x800>'}, 
     :default_style => :index,
     :url => "/assets/projects/:id/:style/:basename.:extension",
     :path => "projects/:id/:style/:basename.:extension",
