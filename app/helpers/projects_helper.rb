47cee4be1e7bc7d9e31ce568718fa08062501d7c
Ryan Michael <kerinin@gmail.com>
2010-04-18 17:02:17 -0500

    adding desaturated mouseover effect

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,9 @@
 module ProjectsHelper
   def project_image( image )
-    image_tag image.attachment.url(:thumb), :size => '55x40', :swapover => image_path( image.attachment.url(:thumb) )
+    image_tag image.attachment.url(:thumb_ds), :size => '55x40', :swapover => image_path( image.attachment.url(:thumb) )
   end
   
   def project_video( video )
-    image_tag video.thumbnail.url(:thumb), :size => '55x40', :swapover => image_path( video.thumbnail.url(:thumb) )
+    image_tag video.thumbnail.url(:thumb_ds), :size => '55x40', :swapover => image_path( video.thumbnail.url(:thumb) )
   end
 end
