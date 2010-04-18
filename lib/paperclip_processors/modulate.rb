ff5da570239f9eb206f61db76e915bc0f2f20a48
Ryan Michael <kerinin@gmail.com>
2010-04-18 16:48:28 -0500

    fixing paperclip bug

--- lib/paperclip_processors/modulate.rb
+++ lib/paperclip_processors/modulate.rb
@@ -1,31 +1,35 @@
 module Paperclip
   class Modulate < Paperclip::Thumbnail
 
+    attr_accessor :brightness, :saturation, :hue
+    
     def transformation
       trans = "-modulate #{@brightness},#{@saturation},#{@hue}"
     end    
     
-    def convert(dst)
-      command = "\"#{ File.expand_path( @thumbnail.path ) }[0]\"\n#{ transformation }\n\"#{ File.expand_path(dst.path) }\"\n" 
+    def convert(src, dst)
+      command = "\"#{ File.expand_path( src.path ) }[0]\"\n#{ transformation }\n\"#{ File.expand_path(dst.path) }\"\n" 
       Paperclip.run('convert',command)
       dst
     end
         
     def initialize(file, options = {}, *args)
-      super file, options, *args
- 
+      super( file, options, *args )
+    
       @brightness = options[:brightness]   ||= 100
       @saturation = options[:saturation]   ||= 100
       @hue        = options[:hue]          ||= 100
     end
   
-    def make *args
-      @thumbnail = super *args
-
-      dst = Tempfile.new([@basename, @format].compact.join("."))
-      dst.binmode    
-      
-      return convert( dst )
+    def make( *args )
+      if (@brightness != 100) || (@saturation != 100) || (@hue != 100)
+        dst = Tempfile.new([@basename, @format].compact.join("."))
+        dst.binmode    
+        
+        return convert( super( *args ), dst )
+      else
+        super *args
+      end
     end
   end
 end
