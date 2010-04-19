c9e285acbdf0add61101d282a08b0d18030c6798
Ryan Michael <ryan@bcarc.com>
2010-04-19 17:10:13 -0500

    video drag works

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -8,7 +8,16 @@ class Admin::VideosController < Admin::BaseController
   create.wants.html { redirect_to edit_admin_project_video_path(@project,@video) }
   update.wants.html { redirect_to edit_admin_project_video_path(@project,@video) }
   destroy.wants.html { redirect_to admin_project_videos_path(@project) }
-  
+
+  def sort
+    @project = Project.find_by_param(params[:id])
+    @project.videos.each do |i|
+      i.position = params["video-list"].index(i.id.to_s)+1
+
+      i.save
+    end
+  end
+    
   private
   
   def collection
