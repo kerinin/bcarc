a883f5fa8665ff2886708de9c9e0d9f61bc73311
Ryan Michael <ryan@bcarc.com>
2010-04-19 17:01:34 -0500

    image dragndrop works

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -10,6 +10,15 @@ class Admin::ImagesController < Admin::BaseController
   update.wants.html { redirect_to edit_admin_project_image_path(@project,@image) }
   destroy.wants.html { redirect_to admin_project_images_path(@project) }
 
+  def sort
+    @project = Project.find_by_param(params[:id])
+    @project.images.each do |i|
+      i.position = params["image-list"].index(i.id.to_s)+1
+
+      i.save
+    end
+  end
+  
   private
   
   def collection
