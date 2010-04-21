4b8939d4bc5476eeac53060c6838a5026f72c8fe
Ryan Michael <kerinin@gmail.com>
2010-04-20 21:59:05 -0500

    resolving globalize/acts_as_markup conflict

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,12 +1,15 @@
 class Page < ActiveRecord::Base
-  #has_permalink :name
   make_permalink :with => :name
   
-  acts_as_wikitext :content
+  #acts_as_wikitext :content
   
   acts_as_list
   
   translates :name, :content
   
   default_scope :order => 'position ASC'
+  
+  def html_content
+    Wikitext::Parser.new().parse( content.to_s )
+  end
 end
