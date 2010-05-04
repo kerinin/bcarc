15df4bf2c118872188df9d8238da9a4dbf039f00
Ryan Michael <ryan@bcarc.com>
2010-05-04 14:10:38 -0500

    fixing that 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
       end
     end
     
-    render :status => 404
+    render :file => "#{RAILS_ROOT}/public/404.html", :status => 404
   end
   
   private
