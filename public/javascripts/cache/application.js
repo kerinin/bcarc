7cd31572779c0999466246dc718b326b54df4bc6
Ryan Michael <ryan@bcarc.com>
2010-05-13 11:03:34 -0500

    fixing some JS, refactoring caching

--- public/javascripts/cache/application.js
+++ public/javascripts/cache/application.js
@@ -1,3 +1,64 @@
+// array( search, replace )
+replaceRules = new Array( '_start', '_over' );
+
+function swapOver(e){
+	if (over = e.target.getAttribute('swapover') ) {
+		e.target.setAttribute('swapOut',e.target.src);
+		e.target.src = over;
+	} else {
+		e.src.src = e.src.src.replace( eval('/'+ replaceRules[0]+'/'), replaceRules[1] );
+	}
+}
+
+function swapOut(e){
+	if (out = e.target.getAttribute('swapout') ) {
+		e.target.src = out;
+	} else {
+		e.src.src = e.src.src.replace( eval('/'+ replaceRules[1]+'/'), replaceRules[0] );
+	}
+}
+
+function fadeOver(e){
+	if(e.target.effect_handle){
+		e.target.effect_handle.cancel();
+	}
+	e.target.setOpacity(0);
+}
+
+function fadeOut(e){
+	e.target.effect_handle = new Effect.Opacity(e.target, { to: 1, duration: 0.5 });
+}
+
+function initCrossFade(start,over_uri){
+	//NOTE: this is a hack to avoid a rounding error in Firefox
+	// it recreates the original image and positions it absolutely so both the mousover and mouseout images are absolutized
+	//insert mouseover image and absolutize
+	start_positioned = Element.insert(start, {after:'<img src="#" />'} ).next();
+	over = Element.insert(start_positioned, {after:'<img src="#" />'} ).next();
+	Element.absolutize(start_positioned);
+	Element.absolutize(over);
+	start_positioned.setStyle({height:start.getStyle('height'),width:start.getStyle('width')});
+	over.setStyle({height:start.getStyle('height'),width:start.getStyle('width')});
+	Element.clonePosition(start_positioned,start,{setWidth:false,setHeight:false});
+	Element.clonePosition(over,start,{setWidth:false,setHeight:false});
+	
+	//set display and src
+	Element.setStyle( start,'opacity:0;' );
+	Element.setStyle( start,'z-index:1;' );
+	Element.setStyle( start_positioned,'z-index:3;' );
+	Element.setStyle( over,'z-index:2;' );
+	start_positioned.src = start.src;
+	over.src = over_uri;
+		
+	//attach behavior
+	jQuery(over).bind('load',[start_positioned,start],function(e) {
+		jQuery(e.data[0]).bind('mouseover',e.data[1], fadeOver).bind('mouseout',e.data[1], fadeOut);
+	} );
+	
+}
+
+
+
 var planCurrentView;
 
 function init_plans(){
@@ -85,4 +146,18 @@ function init_plans(){
 		});
 		
 	});
+}
+
+
+function hideWorkText(e){
+	var container = jQuery(e.target).parents('.work_container')
+	container.children('.work_thumb').fadeTo(200,1);
+	container.children('a.work_text').unbind('mouseleave').hide();
+}
+
+function showWorkText(e){
+	jQuery("a.work_text").hide();
+	var container = jQuery(e.target).parents('.work_container');
+	container.children('.work_thumb').fadeTo(0,.15);
+	container.children('a.work_text').show().mouseleave(hideWorkText);
 }
