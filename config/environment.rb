93fc1171a93934305acaac297f3b372d5d0b9169
Ryan Michael <ryan@bcarc.com>
2010-06-03 16:45:59 -0500

    turns out that gem is required

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem "RedCloth"
   #config.gem "mysql"
   config.gem "memcached"
-  #config.gem "i18n", :version => '0.4.0'
+  config.gem "i18n", :version => '0.4.0'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
