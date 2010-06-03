4f34c34ea40012554e93f020b7bdfcf24557ade1
Ryan Michael <ryan@bcarc.com>
2010-06-03 16:48:53 -0500

    backing out - heroku can't handle the newer i18n

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem "RedCloth"
   #config.gem "mysql"
   config.gem "memcached"
-  config.gem "i18n", :version => '0.4.0'
+  #config.gem "i18n", :version => '0.4.0'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -65,6 +65,6 @@ Rails::Initializer.run do |config|
   }
 end
 
-require "i18n/backend/fallbacks"
+#require "i18n/backend/fallbacks"
 #I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)       # These lines are listed on Stackoverflow but don't seem required
 #I18n.default_locale = "en"
