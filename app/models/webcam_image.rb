04947440d66641b740270c00425cbd8b3f71b826
Ryan Michael <ryan@bcarc.com>
2011-05-12 11:49:31 -0500

    updating webcam to use new file name format (new camera)

--- app/models/webcam_image.rb
+++ app/models/webcam_image.rb
@@ -17,13 +17,17 @@ class WebcamImage < ActiveRecord::Base
                       
   belongs_to :project
   
-  before_create :download_remote_image, :date_from_url
+  before_validation :download_remote_image, :date_from_url, :on => :create
   
   validates_attachment_presence :attachment, :unless => Proc.new {Rails.env == 'test'}
   validates_presence_of :project, :source_url
   
+  #red_bluff_cam20110512_104500M.jpg 12-May-2011 10:45	 34K
+  #red_bluff_cam20110222103815060.jpg 22-Feb-2011 10:38	 45K
   def date_from_url
-    r = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d+)/
+    #r = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d+)/
+    # [year,month,date,hour,minute,second,ms]
+    r = /(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})/
     # [year,month,date,hour,minute,second,ms]
     values = self.source_url.scan(r)
     self.date = DateTime.strptime(values.join(' '), "%Y %m %d %H %M %S")
@@ -44,8 +48,8 @@ class WebcamImage < ActiveRecord::Base
     
     tempfile = Tempfile.new(self.source_url)
     ftp.getbinaryfile(self.source_url, tempfile.path)
-
+    
     self.attachment = tempfile
-    #self.attachment_file_name = self.source_url
+    self.attachment_file_name = self.source_url
   end
 end
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -8,13 +8,19 @@ task :cron => :environment do
   ftp.passive = true
   ftp.login(user = "ftpuser", passwd = "rSW0WstxFTJvNNHP")
   
-  r = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d+).jpg/
+  #r = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d+).jpg/
+  r = /(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(.*).jpg/
   
   #Project.where(:has_webcam => true).each do |project|
   Project.all(:conditions => { :has_webcam => true }).each do |project|
     ftp.chdir(project.webcam_ftp_dir)
     files = ftp.nlst().select {|v| v.include?(project.webcam_file_prefix) && v=~ r && !WebcamImage.exists?(:source_url => v)}
     files.each do |url|
+      
+      image = WebcamImage.new(:project => project, :source_url => url)
+      image.date_from_url
+      image.save!
+      
       begin
         image = WebcamImage.new(:project => project, :source_url => url)
         image.date_from_url
