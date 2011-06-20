a080eaa08e32a2d836b1dae74d78e019361e4958
Ryan Michael <ryan@bcarc.com>
2011-06-20 12:54:43 -0500

    refactoring ajax to use HTML5 history - cleaner now too

--- public/javascripts/prerender.js
+++ public/javascripts/prerender.js
@@ -1,61 +1,68 @@
 // List of HTML content for insertion, indexed by URL
 var content_list = {}
+var current_DOM_path = null
 
-function handle_prerender(e) {
-  // Make sure we're working on the element we expect
-  elem = $(e.target).closest('a[rel=prerender]')
-  
-  // If the requested URL has been cached (should return false if still being cached)
-  if( content = content_list[elem.attr('href')] ) {
-    // Replace the local content with the cached content
-    // Request returns a script with a hash of selector:content pairs
-    for( var selector in content) {
-      $(selector).replaceWith(content[selector]);
+// Load content from a given path
+function load_path( path ) {
+  if( content = content_list[path] ){
+    // If the path has changed, and the content has downloaded (IE, we're not still waiting for it)
+    // The AJAX request triggers a DOM refresh, so we can terminate if it's caching - when it arrives
+    // the callback should take care of the UI.
+    if( path != current_DOM_path && content != 'caching' ) {
+      // Update the DOM
+      for( var selector in content ) {
+        $(selector).replaceWith( content[selector].clone() );
+      }
+      // And keep track of what we're looking at currently
+      current_DOM_path = path;
     }
-    
-    // Make sure the new content has the same behavior
-    update_behavior();
-    
-    // Cache any images that haven't been downloaded yet
-    cache_images();
-    
-    // Don't go follow the link...
-    e.preventDefault();
+  // If the path hasn't been requested, call an ajax request for it
+  } else {
+    cache_path(path)
   }
 }
 
-function update_behavior(){
-  $("a[rel=prerender]").click(handle_prerender);
+function cache_path(path) {
+  if( !content_list[path] ){
+    // Request the path, when it get back make sure the UI is updated (possible that it'll be requested during the page load)
+    $.getScript(path, refresh_DOM);
+    content_list[path] = 'caching';
+  }
 }
 
-function cache_image( elem ) {
-  var href = $(elem).attr('href');
-  
-  // Only cache if a request hasn't been sent
-  if( !content_list[href] ) {
-    content_list[ href ] = null;
-    // Otherwise, send and XHR request for the URL listed
-    $.getScript( href );
-  }  
+function refresh_DOM() {
+  load_path(window.location.pathname);
+  cache_images();
 }
 
 function cache_images( scope ) {
   // Cache the next 2 images
   $('a.current_image').nextAll('a[rel=prerender]:lt(4)').each( function(i,elem) {
-    cache_image(elem);
+    cache_path( $(elem).attr('href') );
   });  
   // And cache the first 2 images
   $('a[rel=prerender]:lt(2)').each( function(e,elem) {
-    cache_image(elem);  
+    cache_path( $(elem).attr('href') );  
   });
 }
 
 $(document).ready( function() {
+  $.htmlhistory.init({ interceptLinks: false });
+  
+  $('body').delegate('a[rel=prerender]', 'click', function(e) {
+    $.htmlhistory.changeTo( $(this).attr('href') );
+    e.preventDefault();
+  })
+  
+  $(window).bind('htmlhistory', function(e) {
+    load_path( window.location.pathname);
+  });
+  
   // Cache the current page
+  current_DOM_path = window.location.pathname;
   content_list[window.location.pathname] = {
     ".project_header.thumb_container" : $(".project_header.thumb_container").clone(),
     ".content" : $(".content").clone()
   }
-  update_behavior();
   cache_images();
 });
