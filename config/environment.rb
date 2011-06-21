5aa819aac54b3fbff9876f829b042f62fafdc382
Ryan Michael <ryan@bcarc.com>
2011-06-21 15:39:44 -0500

    updating gems (trying to fix mem leak), porting layout to haml

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+require 'bleak_house' if ENV['BLEAK_HOUSE']
+
 RoutingFilter::Locale.include_default_locale = false
 
 # Initialize the rails application
