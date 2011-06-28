ac8ae8703df9faa0d7486205f1f5178eb2e102c9
Ryan Michael <ryan@bcarc.com>
2011-06-28 17:20:23 -0500

    working on a JS refactor in hopes of getting the back button to work

--- public/javascripts/prerender.js
+++ public/javascripts/prerender.js
@@ -1,96 +1,13 @@
-// If the URL has been reloaded from a gimpy browser (url#/url), redirect before it renders
-if( window.location.hash ) window.location.href = window.location.hash.replace(/^#/,'');
-
 // List of HTML content for insertion, indexed by URL
-var content_list = {}
-var current_DOM_path = null
-
-/*
-I think we're going to want to anchor to the opposite side we're inserting on.
-
-So, if we're adding an image on the right, we anchor to the left,
-insert the content, the animate the left property
-
-If we're adding an image on the left, we anchor on the right, insert
-the content on the left, then animate the right property.
-
-If we've only ever got two panes, we can probably set left/right to 0
-*/
-
-function slide_left() {
-  $('.content').animate( {left: '-=800'}, 1000 );
-}
-
-function slide_right() {
-  $('.content').animate( {left: '+=800'}, 1000 );
-}
-
-function slide_spinner_from_left() {
-  $('.content').removeClass('swap_target');
-  
-  $('.slide_box').css({'left': null, 'right': 0}).append( 
-    $('.spinner.hidden').clone().removeClass('hidden').addClass('current')
-  );
-  $('.slide_box > .spinner').wrap("<div class='content swap_target' style='position:absolute;right:800px;top:0px;'>");
-  
-  $('.slide_box > *').animate({'right' : '-=800'}, 1000, 'swing', function(){
-    $('.content').not('.swap_target').remove();
-    $('.content').removeAttr("style");
-  });  
-}
-
-function slide_spinner_from_right() {
-  $('.content').removeClass('swap_target');
-  
-  $('.slide_box').css({'left': 0, 'right': null}).append( 
-    $('.spinner.hidden').clone().removeClass('hidden').addClass('current')
-  );
-  $('.slide_box > .spinner').wrap("<div class='content swap_target' style='position:absolute;left:800px;top:0px;'>");
-  
-  $('.slide_box > *').animate({'left': '-=800'}, 1000, 'swing', function(){
-    $('.content').not('.swap_target').remove();
-    $('.content').removeAttr("style");
-  });
-}
-
-function show_spinner() {
-  $('.content').replaceWith( $('.spinner.hidden').clone().removeClass('hidden').addClass('current') );
-  $('.spinner.current').wrap("<div class='content'>");
-}
-
-// Load content from a given path
-function load_path( path ) {
-  if( content = content_list[path] ){
-    // If the path has changed, and the content has downloaded (IE, we're not still waiting for it)
-    // The AJAX request triggers a DOM refresh, so we can terminate if it's caching - when it arrives
-    // the callback should take care of the UI.
-    if( path != current_DOM_path && content != 'caching' ) {
-      // Update the DOM
-      for( var selector in content ) {
-        $(selector).replaceWith( content[selector].clone() );
-      }
-      // And keep track of what we're looking at currently
-      current_DOM_path = path;
-    }
-  // If the path hasn't been requested, call an ajax request for it
-  } else {
-    show_spinner();
-    cache_path(path);
-  }
-}
+var content_list = {};
+var DOM_state = null;
 
 function cache_path(path) {
   if( !content_list[path] ){
     // Request the path, when it get back make sure the UI is updated (possible that it'll be requested during the page load)
-    $.getScript(path, refresh_DOM);
+    $.getScript(path, insert_content);
     content_list[path] = 'caching';
   }
-}
-
-function refresh_DOM() {
-  if( window.location.hash ) load_path( window.location.hash.replace(/^#/,'') );
-  else load_path( window.location.href);
-  cache_images();
 }
 
 function cache_images( scope ) {
@@ -104,29 +21,91 @@ function cache_images( scope ) {
   });
 }
 
+function insert_content() {
+  var State = History.getState();
+  var path = State.url;
+  
+  if( path != DOM_state ) {
+    if( content = content_list[path] ) {
+      for( var selector in content ) {
+        $(selector).replaceWith( content[selector].clone() );
+      }
+    } else {
+      $('.content.swap_target').replaceWith( $('.spinner.hidden').clone().removeClass('hidden').addClass('current') );
+      $('.spinner.current').wrap("<div class='content'>");      
+    }
+    DOM_state = path;
+  }
+}
+
+function prepare_for_transition( name ) {
+  if( name == 'from_right' ) { 
+    $('.content').removeClass('swap_target');
+    $('.slide_box').css({'left':0,'right':null}).append( $("<div class='content swap_target' style='position:absolute;left:800px;top:0px;'></div>") )
+  } else if( name == 'from_left' ) {
+    $('.content').removeClass('swap_target');
+    $('.slide_box').css({'left':null, 'right':0}).append( $("<div class='content swap_target' style='position:absolute;right:800px;top:0px;'></div>"))
+  }
+}
+
+function animation_for( transition ) {
+  if( transition == 'from_right' ) {
+    $('.slide_box > *').animate({'left': '-=800'}, 1000, 'swing', function(){
+      $('.content').not('.swap_target').remove();
+      $('.content').removeAttr("style");
+    });      
+  } else if( transition == 'from_left' ) {
+    $('.slide_box > *').animate({'right' : '-=800'}, 1000, 'swing', function(){
+      $('.content').not('.swap_target').remove();
+      $('.content').removeAttr("style");
+    });
+  }
+}
+
 function init() {
+  
+  var History = window.History;
+  
+  // Behavior when URL is changed
+  $(window).bind('statechange',function(e){
+    var State = History.getState();
+    var path = State.url;
+    var transition = null;
+    if( path == $('#next_button a[rel=prerender]').attr('href') ) { 
+      transition = 'from_right';
+    } else if( path == $('#prev_button a[rel=prerender]').attr('href') ) {
+      transition = 'from_left';
+    }    
+    
+    prepare_for_transition( transition );
+    
+    /*
+    Inserting the content clears out the style attributes that are being animate, short circuiting the
+    whole animation.  Probably need to dynamically wrap the content in an animation container and then
+    unwrap it (and delete the old content) when the transition is over.
+    */
+    insert_content();
+    
+    //animation_for( transition );
+  });
+  
+  // State change triggers
+  $('body').delegate('a[rel=prerender]', 'click', function(e) {
+    History.pushState(null, null, $(this).attr('href') );
+    e.preventDefault();
+  })
+  
+  cache_images();  
+}
+
+$(document).ready( function() {
   // Cache the current page
-  current_DOM_path = window.location.href;
+  DOM_state = window.location.href;
   content_list[window.location.href] = {
     ".project_header.thumb_container" : $(".project_header.thumb_container").clone(),
     ".content" : $(".content").clone()
   }
   
-  // Init the history
-  $.htmlhistory.init({ interceptLinks: false });    
-
-  // Bind behaviors to the history plugin
-  $('body').delegate('a[rel=prerender]', 'click', function(e) {
-    $.htmlhistory.changeTo( $(this).attr('href') );
-    e.preventDefault();
-  })
-  $(window).bind('htmlhistory', refresh_DOM);
-
-  // Cache the next couple images
-  cache_images();  
-}
-
-$(document).ready( function() {
   // This keeps the browser spinner from going while the ajax loads
   // May also avoid some page loads if people click back quickly
   var t=setTimeout(init,1500);
