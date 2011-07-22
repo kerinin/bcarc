79935c07895aa355a7f56f9a70da1c33084935dc
Ryan Michael <ryan@bcarc.com>
2011-07-22 12:18:33 -0500

    more

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
-require 'openid/store/filesystem'
+#require 'openid/store/filesystem'
+require 'openid/store/memcache'
 
 #Rails.application.config.middleware.use OmniAuth::Strategies::GoogleApps, OpenID::Store::Filesystem.new('./tmp'), :name => 'admin', :domain => 'bcarc.com'
-Rails.application.config.middleware.use OmniAuth::Strategies::GoogleApps, nil, :name => 'admin', :domain => 'bcarc.com'
+Rails.application.config.middleware.use OmniAuth::Strategies::GoogleApps, OpenID::Store::Memcache.new(Dalli::Client.new), :name => 'admin', :domain => 'bcarc.com'
