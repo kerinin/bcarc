d5b0092e5eee8eeab135e8a2f3d798edbffb306a
Ryan Michael <ryan@bcarc.com>
2011-07-22 12:15:07 -0500

    trying no filesystem for oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 require 'openid/store/filesystem'
 
-Rails.application.config.middleware.use OmniAuth::Strategies::GoogleApps, OpenID::Store::Filesystem.new('./tmp'), :name => 'admin', :domain => 'bcarc.com'
+#Rails.application.config.middleware.use OmniAuth::Strategies::GoogleApps, OpenID::Store::Filesystem.new('./tmp'), :name => 'admin', :domain => 'bcarc.com'
+Rails.application.config.middleware.use OmniAuth::Strategies::GoogleApps, nil, :name => 'admin', :domain => 'bcarc.com'
