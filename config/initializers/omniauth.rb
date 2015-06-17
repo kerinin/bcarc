9791bbaace70c9e99b489b9d70ec0229f995dce5
Ryan Michael <kerinin@gmail.com>
2015-06-17 15:10:50 -0500

    fuck

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,6 @@ OpenID.fetcher.ca_file = "#{Rails.root}/cacert.crt"
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :openid, :store => OpenID::Store::Filesystem.new('./tmp')
-
-  use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('./tmp'), :name => 'openid', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :open_id, :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'openid', :identifier => 'https://www.google.com/accounts/o8/id'
 end
 
