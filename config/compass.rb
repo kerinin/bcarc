21560c19adf42fde37da409d401b7e2ff97469b8
Ryan Michael <kerinin@gmail.com>
2015-06-17 20:17:57 -0500

    meh

--- config/compass.rb
+++ config/compass.rb
@@ -1,12 +1,13 @@
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 project_type = :rails
-project_path = Compass::AppIntegration::Rails.root
+project_path = Rails.root
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 #css_dir = "public/stylesheets/compiled"
 css_dir = 'tmp/stylesheets/compiled'
 sass_dir = "app/stylesheets"
-environment = Compass::AppIntegration::Rails.env
+environment = Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
